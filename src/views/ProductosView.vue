4ad0caf8289ada47e024b6128c03293ff57d385c
Grabiel <grabielneyrad@gmail.com>
2026-05-27 23:40:34 -0300

    Agregue el tienda.json y modifique un poco la pagina de los productos

--- src/views/ProductosView.vue
+++ src/views/ProductosView.vue
@@ -1,13 +1,17 @@
 <script setup>
-import { ref } from 'vue'
+// import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
+import productos from '../tienda.json' // ← importa el JSON directo
 
-const productos = ref([])
-
-fetch('https://fakestoreapi.com/products')
-  .then((res) => res.json())
-  .then((data) => (productos.value = data))
-  .catch((err) => console.log(err))
+// Cuando quieras pasar a la API real, comentá la línea de arriba
+// y descomentá esto:
+// const productos = ref([])
+// fetch('https://fakestoreapi.com/products')
+//   .then((res) => res.json())
+//   .then((data) => (productos.value = data))
+//   .catch((err) => console.log(err))
 </script>
+
 <template>
   <h1>Productos</h1>
   <section class="">
@@ -53,14 +57,35 @@ fetch('https://fakestoreapi.com/products')
     </div>
     <div>
       <div v-for="producto in productos" :key="producto.id">
-        <a href="/detalle/">
+        <RouterLink :to="`/detalle/${producto.id}`">
+          <span v-if="!(producto.tag == null)">{{ producto.tag }}</span>
           <figure>
-            <img :src="producto.image" :alt="producto.title" />
+            <img :src="producto.image" :alt="producto.name" />
           </figure>
           <div>
-            <p>{{ producto.title }}</p>
+            <p>{{ producto.name }}</p>
+            <p>{{ producto.price }}</p>
+            <div>
+              <button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                  />
+                </svg>
+                Agregar
+              </button>
+            </div>
           </div>
-        </a>
+        </RouterLink>
       </div>
     </div>
   </section>
